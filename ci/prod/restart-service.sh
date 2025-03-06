570e2bf05002b4466b8136cea1cea45d8e2c6dea
zimeng <45909848+zimeng1@users.noreply.github.com>
2025-03-06 15:48:02 +0800

    Delete ci directory

--- ci/prod/restart-service.sh
+++
@@ -1,3 +0,0 @@
-export HOST_IP=$(hostname -I | cut -d' ' -f1)
-cd /opt/app/mc-payment-service && sudo docker build -t mc-payment-service . --no-cache
-cd /opt/app/mc-payment-service && docker-compose -f docker-compose.yaml up -d mc-payment-service
